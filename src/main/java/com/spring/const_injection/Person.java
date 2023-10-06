d084aaf68ac886f210b7ac7d2f2996760ced4148
Kishore1K <reddykishore527@gmail.com>
2023-10-06 19:24:13 +0530

    Constructor Injection

--- src/main/java/com/spring/const_injection/Person.java
+++ src/main/java/com/spring/const_injection/Person.java
@@ -1,9 +1,13 @@
 package com.spring.const_injection;
 
+import java.util.List;
+
 public class Person {
     private String name;
     private  Long personId;
     private  Certif certif;
+
+    private List<String> list;
 
     @Override
     public String toString() {
@@ -11,13 +15,15 @@ public class Person {
                 "name='" + name + '\'' +
                 ", personId=" + personId +
                 ", certif=" + certif +
+                ", list=" + list +
                 '}';
     }
 
-    public Person(String name, Long personId, Certif cert) {
+    public Person(String name, Long personId, Certif cert, List<String> list) {
         this.name = name;
         this.personId = personId;
         this.certif = cert;
 
+        this.list = list;
     }
 }
