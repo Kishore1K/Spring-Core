685e01b142621d8d7568bf7a4cc7ab744d31b8c9
Kishore1K <reddykishore527@gmail.com>
2023-10-07 12:01:44 +0530

    Autowire By XML -byName

---
+++ src/main/java/com/spring/auto/wire/Emp.java
@@ -0,0 +1,32 @@
+package com.spring.auto.wire;
+
+public class Emp {
+    private String name;
+    private Address address;
+
+    @Override
+    public String toString() {
+        return "Emp{" +
+                "name='" + name + '\'' +
+                ", address=" + address +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+
+}
