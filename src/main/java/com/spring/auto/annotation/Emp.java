0ac430882980e3f5c704b62a0983c29ea2bddb4e
Kishore1K <reddykishore527@gmail.com>
2023-10-07 12:27:08 +0530

    Autowire By @AutoWire

---
+++ src/main/java/com/spring/auto/annotation/Emp.java
@@ -0,0 +1,46 @@
+package com.spring.auto.annotation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Emp {
+    private String name;
+    @Autowired
+    private Address address;
+
+    public Emp() {
+        super();
+    }
+
+//    @Autowired
+    public Emp(String name, Address address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Emp{" +
+                "name='" + name + '\'' +
+                ", address=" + address +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+//    @Autowired
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+
+}
